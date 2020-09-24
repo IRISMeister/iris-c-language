c76adfd0a9bed80683a64b4e9be1f5a4a418dbf2
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-09-24 11:20:36 +0900

    fix

--- src/compile.sh
+++ src/compile.sh
@@ -1,7 +1,7 @@
 echo "Compiling..."
-gcc callin_misc.c callin_misc_calls.c -I /usr/irissys/dev/iris-callin/include -Wl,-rpath,/usr/irissys/bin -L/usr/irissys/bin -lirisdbt -o callin_misc -g3 -O0
+gcc callin_misc.c callin_misc_calls.c callin_misc_value.c callin_execute.c -I /usr/irissys/dev/iris-callin/include -Wl,-rpath,/usr/irissys/bin -L/usr/irissys/bin -lirisdbt -o callin_misc -g3 -O0
 gcc callin_multithreads.c -I /usr/irissys/dev/iris-callin/include -Wl,-rpath,/usr/irissys/bin -L/usr/irissys/bin -lirisdbt -pthread -o callin_multithreads -g3 -O0
-gcc callin_unicode_value_test.c -I /usr/irissys/dev/iris-callin/include -Wl,-rpath,/usr/irissys/bin -L/usr/irissys/bin -lirisdbt -o callin_unicode_value_test -g3 -O0
+gcc callin_repeat.c callin_misc_calls.c -I /usr/irissys/dev/iris-callin/include -Wl,-rpath,/usr/irissys/bin -L/usr/irissys/bin -lirisdbt -o callin_repeat -g3 -O0
 
 gcc callout.c -fPIC -I /usr/irissys/dev/iris-callin/include -shared -o callout.so -g3 -O0
 
